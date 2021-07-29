d50c053f1c7bf99f9fd88447b32e9e7ce937937c
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-29 22:42:45 +0200

    Change website name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'C++ Best Practices',
+  tagline: 'A gentle introduction',
   url: 'https://julesfouchy.github.io/Learn--Cpp-Best-Pratices',
   baseUrl: '/Learn--Cpp-Best-Pratices/',
   onBrokenLinks: 'throw',
