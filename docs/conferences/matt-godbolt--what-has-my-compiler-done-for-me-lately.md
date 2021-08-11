b68b50b56764ac97759589bdded5266af13e7c51
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-11 13:54:11 +0200

    Update

---
+++ docs/conferences/matt-godbolt--what-has-my-compiler-done-for-me-lately.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=bSkpMdDe4g4
+
+Amazing optimizations done by the compilers, and a bit about understanding assembly code.
--- evaluation/skills.ts
+++ evaluation/skills.ts
@@ -1,27 +1,35 @@
-import { Skill } from '../Thoughts-on-Teaching/evaluation/Skill.ts'
+import {Skill} from '../Thoughts-on-Teaching/evaluation/Skill.ts'
 
-const skills: Skill[] = [
-    {
-        shortname: "const correctness",
-        description: "Yo",
+const skills: Skill[] =
+    [
+      {
+        shortname: 'Use const correctly',
+        description: 'Yo',
         difficulty: 0,
         benefit: 0,
         importance: 5,
-    },
-    {
-        shortname: "templates",
-        description: "templateSS",
+      },
+      {
+        shortname: 'templates',
+        description: 'templateSS',
         difficulty: 3,
         benefit: 3,
         importance: 4,
-    },
-    {
-        shortname: "use {} for initialization",
-        description: "",
+      },
+      {
+        shortname: 'Use {} for initialization',
+        description: '',
         difficulty: 2,
         benefit: 2,
         importance: 4,
-    },
-]
+      },
+      {
+        shortname: 'Use Git',
+        description: '',
+        difficulty: 1,
+        benefit: 5,
+        importance: 4,
+      },
+    ]
 
-export default skills
+    export default skills
