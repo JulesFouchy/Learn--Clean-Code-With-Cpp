42f71486f9fb8071063b4cea7486168385a207f5
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-12 17:06:32 +0200

    Sort skills by priority

--- src/components/SkillsTable.js
+++ src/components/SkillsTable.js
@@ -2,21 +2,29 @@ import React from 'react'
 import skills from '@site/.docusaurus/gather-skills-plugin/default/friends.json'
 import skill_priority from '../../Thoughts-on-Teaching/evaluation/skill_priority'
 
-export default () => (
-    <table>
-        <tr>
-            <th>Skill</th>
-            <th>Benefit</th>
-            <th>Easiness</th>
-            <th>Importance</th>
-        </tr>
-        {skills.map(skill =>
+export default () => {
+    const prioritized_skills = skills.map(skill => ({
+        ...skill,
+        priority: skill_priority(skill.value)
+    }))
+    .sort((a, b) => a.priority < b.priority)
+    
+    return (
+        <table>
             <tr>
-                <td><a href = {skill.description.link}>{skill.description.name}</a></td>
-                <td>{skill.value.benefit}</td>
-                <td>{skill.value.easiness}</td>
-                <td>{skill_priority(skill.value)}</td>
+                <th>Skill</th>
+                <th>Benefit</th>
+                <th>Easiness</th>
+                <th>Priority</th>
             </tr>
-        )}
-    </table>
-)
+            {prioritized_skills.map(skill =>
+                <tr>
+                    <td><a href = {skill.description.link}>{skill.description.name}</a></td>
+                    <td>{skill.value.benefit}</td>
+                    <td>{skill.value.easiness}</td>
+                    <td>{skill.priority}</td>
+                </tr>
+            )}
+        </table>
+    )
+}
