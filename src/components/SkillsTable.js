6a05431f973e8c2650b7b9499f09c3a1c2f3f867
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-15 01:12:44 +0200

    Display tags nicely

--- src/components/SkillsTable.js
+++ src/components/SkillsTable.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import skills from '@site/.docusaurus/gather-skills-plugin/default/skills.json'
 import skill_priority from '../../Thoughts-on-Teaching/evaluation/skill_priority'
+import style from './SkillsTable.module.css'
+
+const tags = (tags_list) =>
+    tags_list.map(tag => (
+        <div className={style.tag}>{tag}</div>
+    ))
 
 export default () => {
     const prioritized_skills = skills.map(skill => ({
@@ -13,20 +19,20 @@ export default () => {
         <table>
             <tr>
                 <th>Skill</th>
+                <th>Tags</th>
+                <th>Importance</th>
                 <th>Benefit</th>
                 <th>Easiness</th>
                 <th>Order</th>
-                <th>Importance</th>
-                <th>Tags</th>
             </tr>
             {prioritized_skills.map(skill =>
                 <tr>
                     <td><a href = {skill.link}>{skill.title}</a></td>
+                    <td>{tags(skill.tags || [])}</td>
+                    <td>{skill.priority}</td>
                     <td>{skill.benefit}</td>
                     <td>{skill.easiness}</td>
                     <td>{skill.order}</td>
-                    <td>{skill.priority}</td>
-                    <td>{skill.tags}</td>
                 </tr>
             )}
         </table>
