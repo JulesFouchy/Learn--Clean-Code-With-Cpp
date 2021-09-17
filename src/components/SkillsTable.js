9355bfb9809a6623f86006cc4a507a91affeb507
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-17 22:40:07 +0200

    [SkillsTable] Start checking checkboxes based on student's skills

--- src/components/SkillsTable.js
+++ src/components/SkillsTable.js
@@ -3,7 +3,46 @@ import skills from '@site/.docusaurus/gather-skills-plugin/default/skills.json'
 import skill_priority from '../../Thoughts-on-Teaching/evaluation/skill_priority'
 import style from './SkillsTable.module.css'
 import Checkbox from '@mui/material/Checkbox'
-import { green } from '@mui/material/colors'
+import { green, blueGrey } from '@mui/material/colors'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const student_skills = {
+    old: {
+        "use-a-formatting-tool": 1,
+    },
+    new: {
+        "code-reviews": 1,
+        "against-nested-namespaces": 1,
+    },
+}
+
+const students_dropdown = () => {
+    const [student, set_student] = React.useState('')
+    const handle_change = (event) => {
+        set_student(event.target.value)
+        current_student = student
+    }
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Student</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={student}
+                    label="Student"
+                    onChange={handle_change}
+                >
+                {Object.keys(students).map((student, i) => <MenuItem value={i}>{student}</MenuItem>)}
+                </Select>
+            </FormControl>
+        </Box>
+      );
+}
 
 const tags = (tags_list) =>
     tags_list.map(tag => (
@@ -20,6 +59,16 @@ const checkbox_validated = () =>
         }}
     />
 
+const checkbox_validated_disabled = () => 
+    <Checkbox
+        checked
+        disabled
+        sx={{'&.Mui-checked': {
+                color: blueGrey[300],
+            },
+        }}
+    />
+
 const checkbox_not_validated = () => 
     <Checkbox
     />
@@ -32,27 +81,32 @@ export default () => {
     .sort((a, b) => a.priority < b.priority)
     
     return (
-        <table>
-            <tr>
-                <th>Skill</th>
-                <th>Validated</th>
-                <th>Tags</th>
-                <th>Importance</th>
-                <th>Benefit</th>
-                <th>Easiness</th>
-                <th>Order</th>
-            </tr>
-            {prioritized_skills.map(skill =>
+        <div>
+            {/* {students_dropdown()} */}
+            <table>
                 <tr>
-                    <td><a href = {skill.link}>{skill.title}</a></td>
-                    <td>{skill.order === 1 ? checkbox_validated() : checkbox_not_validated()}</td>
-                    <td>{tags(skill.tags || [])}</td>
-                    <td>{skill.priority}</td>
-                    <td>{skill.benefit}</td>
-                    <td>{skill.easiness}</td>
-                    <td>{skill.order}</td>
+                    <th>Skill</th>
+                    <th>Validated</th>
+                    <th>Tags</th>
+                    <th>Importance</th>
+                    <th>Benefit</th>
+                    <th>Easiness</th>
+                    <th>Order</th>
                 </tr>
-            )}
-        </table>
+                {prioritized_skills.map(skill =>
+                    <tr>
+                        <td><a href = {skill.link}>{skill.title}</a></td>
+                        <td>{ student_skills.new[skill.slug] ? checkbox_validated()
+                            : student_skills.old[skill.slug] ? checkbox_validated_disabled()
+                            :                                  checkbox_not_validated()}</td>
+                        <td>{tags(skill.tags || [])}</td>
+                        <td>{skill.priority}</td>
+                        <td>{skill.benefit}</td>
+                        <td>{skill.easiness}</td>
+                        <td>{skill.order}</td>
+                    </tr>
+                )}
+            </table>
+        </div>
     )
 }
