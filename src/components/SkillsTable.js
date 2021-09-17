771d435bbba5aa63be759d509cb3e09b7330ab05
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-17 22:09:42 +0200

    Add checkboxes for the skills

--- src/components/SkillsTable.js
+++ src/components/SkillsTable.js
@@ -2,11 +2,27 @@ import React from 'react'
 import skills from '@site/.docusaurus/gather-skills-plugin/default/skills.json'
 import skill_priority from '../../Thoughts-on-Teaching/evaluation/skill_priority'
 import style from './SkillsTable.module.css'
+import Checkbox from '@mui/material/Checkbox'
+import { green } from '@mui/material/colors'
 
 const tags = (tags_list) =>
     tags_list.map(tag => (
         <div className={style.tag}>{tag}</div>
     ))
+
+const checkbox_validated = () => 
+    <Checkbox
+        checked
+        disabled
+        sx={{'&.Mui-checked': {
+                color: green[600],
+            },
+        }}
+    />
+
+const checkbox_not_validated = () => 
+    <Checkbox
+    />
 
 export default () => {
     const prioritized_skills = skills.map(skill => ({
@@ -19,6 +35,7 @@ export default () => {
         <table>
             <tr>
                 <th>Skill</th>
+                <th>Validated</th>
                 <th>Tags</th>
                 <th>Importance</th>
                 <th>Benefit</th>
@@ -28,6 +45,7 @@ export default () => {
             {prioritized_skills.map(skill =>
                 <tr>
                     <td><a href = {skill.link}>{skill.title}</a></td>
+                    <td>{skill.order === 1 ? checkbox_validated() : checkbox_not_validated()}</td>
                     <td>{tags(skill.tags || [])}</td>
                     <td>{skill.priority}</td>
                     <td>{skill.benefit}</td>
