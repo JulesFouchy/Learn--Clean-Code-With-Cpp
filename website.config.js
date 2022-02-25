839a06e3f2822ae0ae4d66d92d7dc47d94d894ed
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-25 14:38:29 +0100

    [website] Update link to my website

--- website.config.js
+++ website.config.js
@@ -44,7 +44,7 @@ module.exports = async () => {
       },
     ],
     classes: classes,
-    copyright_before: `These lessons were written by <a href="https://julesfouchy.github.io/home/">Jules Fouchy</a> and <a href="https://guillaumehaerinck.com/#/">Guillaume Haerinck</a><br/>`,
+    copyright_before: `These lessons were written by <a href="https://julesfouchy.github.io/">Jules Fouchy</a> and <a href="https://guillaumehaerinck.com/#/">Guillaume Haerinck</a><br/>`,
     copyright_after: `<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/justicon" title="justicon">justicon</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>`,
     syntax_highlight_for: ["cmake", "csharp"],
     lessons_allowed_tags: [
